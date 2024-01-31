f9872cab57968c98f57fe2d135207909aacb42a2
Aniol <aniol0012@gmail.com>
2024-01-31 18:20:22 +0100

    refactor: improved ex 3 p1 2023

--- Parcial1/2023/ex3_2023_p1.java
+++ Parcial1/2023/ex3_2023_p1.java
@@ -6,7 +6,13 @@ public class ex3_2023_p1 {
     public static void main(String[] args) {
         var elems = List.of("a", "b", "c", "d", "e", "f", "g");
         var list = new ArrayList<>(elems);
+        System.out.println("V1");
         removeInPairPosition(list);
+        System.out.println(list);
+
+        list = new ArrayList<>(elems);
+        System.out.println("V2");
+        removeInPairPosition_optimized(list);
         System.out.println(list);
     }
 
@@ -23,4 +29,19 @@ public class ex3_2023_p1 {
         }
     }
 
+    static <E> void removeInPairPosition_optimized(List<E> elements) {
+        assert elements != null;
+        List<E> result = new ArrayList<>();
+        Iterator<E> it = elements.iterator();
+        int index = 0;
+        while (it.hasNext()) {
+            E elem = it.next();
+            if (index % 2 != 1) {
+                result.add(elem);
+            }
+            index++;
+        }
+        elements.clear();
+        elements.addAll(result);
+    }
 }

ed13be3875771c9d6870043ed6eb732a016f4f8f
Aniol <aniol0012@gmail.com>
2024-02-02 14:46:10 +0100

    feat: new drop version

--- Parcial1/2023/ex2_2024_p1_rec.java
+++ Parcial1/2023/ex2_2024_p1_rec.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class ex2_2024_p1_rec {
 
@@ -45,13 +42,44 @@ public class ex2_2024_p1_rec {
         }
     }
 
+    // O(n)
+    public static <E> void drop_v3(List<E> elements, int n) {
+        assert elements != null;
+        assert n >= 0;
+        if (n < 0 || n > elements.size()) {
+            throw new IllegalArgumentException();
+        }
+        int size = elements.size();
+        for (int i = n; i < size; i++) {
+            elements.set(i - n, elements.get(i));
+        }
+        for (int i = 0; i < n; i++) {
+            // This can be done with removeLast
+            elements.remove(elements.size() - 1);
+        }
+    }
+
     public static void main(String[] args) {
         List<String> list = new ArrayList<>(List.of("a", "b", "c", "d", "e", "f", "g", "h"));
-        drop(list, 4);
-        System.out.println("list = " + list);
-
         List<String> list2 = new ArrayList<>(List.of("a", "b", "c", "d", "e", "f", "g", "h"));
-        drop_v2(list2, 4);
-        System.out.println("list2 = " + list2);
+        List<String> list3 = new ArrayList<>(List.of("a", "b", "c", "d", "e", "f", "g", "h"));
+
+        // Measure and print the execution time for each drop method
+        measureAndPrintTime(() -> drop(list, 4), "drop");
+        System.out.println("list after drop = " + list);
+
+        measureAndPrintTime(() -> drop_v2(list2, 4), "drop_v2");
+        System.out.println("list2 after drop_v2 = " + list2);
+
+        measureAndPrintTime(() -> drop_v3(list3, 4), "drop_v3");
+        System.out.println("list3 after drop_v3 = " + list3);
+    }
+
+    public static void measureAndPrintTime(Runnable task, String taskName) {
+        long startTime = System.nanoTime();
+        task.run();
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Execution time of " + taskName + " (nanoseconds): " + duration);
     }
 }
